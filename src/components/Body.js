561fa808c04b5d91604cd5aac42d5a12048cc4c1
gauravchauhan2403 <gauravchauhan2403@gmail.com>
2026-01-04 17:46:45 +0530

    updated Best restraunt algorithm a little to update between all and best based on click count.

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,17 +3,25 @@ import resData from "../utils/mockData";
 import { useState } from "react";
 
 const Body = () => {
-  let [filteredResData, setFilterResData] = useState(resData)
+  let [filteredResData, setFilterResData] = useState(resData);
+  let [count, setCount] = useState(0);
 
   return (
     <>
-      <button onClick={ () => {           // for filteringout data above rating 4
+      <button onClick={(count %2 ==0) ? (
+        () => {           // for filteringout data above rating 4
         const filterData = filteredResData.filter((res) => res.avgRating > 4);
         setFilterResData(filterData);
-        }} 
+        setCount(count+1);
+        }) : (
+          () => { 
+          setFilterResData(resData);
+          setCount(count+1);
+        })
+      } 
         style={{ border: "1px solid white", cursor: "pointer"}}
       >
-        Best Restraunt
+        {(count % 2 ==0)?"Best restraunt": "All restraunt"}
       </button>
       <div className="body_container">
         {filteredResData.map((restraunt) => (
