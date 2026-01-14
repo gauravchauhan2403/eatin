55b1c53762f184b9edc4cf75cd8709044d5f7e69
gauravchauhan2403 <gauravchauhan2403@gmail.com>
2026-01-14 17:24:33 +0530

    created about component with class component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import {useLoaderData} from "react-router"
 
-const About = () => {
-  return (
-    <div>About Page</div>
-  )
+class About extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      endorse: false,
+      userInfo: {dummy: "dummyvalue"}
+    };
+  }
+
+  async componentDidMount(){
+    console.log("used for Api fetching and is called once after the render, also it will be called after render method")
+
+    // const data = await fetch("dummy api");
+    // const JsonData = await data.json();
+    // console.log(JsonData)
+
+    // this.setState({
+    //   userInfo: JsonData
+    // });
+  }
+
+  componentDidUpdate(){
+    console.log("called on each time the component is rendered, and checks change of states")
+  }
+
+  componentWillUnmount(){
+    console.log(" it is called just before the component is unmouted and is used to clear the messcreated");
+    console.log("used for clearing intervals etc")
+  }  
+
+  render() {
+    return(
+      <div className='Container'>
+        <span>Name: Gaurav</span>
+        <span>location: Delhi</span>
+        <button className="endore" onClick = {()=>{
+          this.setState({endorse: !this.state.endorse});
+        }}
+        >{this.state.endorse ? "Endorsed" : "Endorse"}</button>
+      </div>
+    )
+    
+  }
 }
 
 export default About
